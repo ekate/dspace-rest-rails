8acfbcb6b30e0e390882f0ef7eaca7dd9d959e16
Mike Kalvas <kalvas.2@osu.edu>
2014-03-13 17:32:10 -0400

    Init rails dspace rest consumer

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,37 @@
+class HomeController < ApplicationController
+
+  require 'json'
+  require 'rest_client'
+
+  def index
+
+    @communities = Array.new
+    baseRestURL = "http://kb.osu.edu/rest"
+
+    url = urlAppend("communities", baseRestURL)
+
+    rawJSON = RestClient.get url, {:accept => :json}
+    jsonNode = JSON.parse(rawJSON)
+
+    i = 0
+    while i < jsonNode.size
+
+      @communities.push jsonNode[i]['name']
+      i += 1
+
+    end
+
+    #@communities.to_crash
+
+    return @communities
+
+  end
+
+  def urlAppend(endpoint, baseURL)
+
+    finalURL = baseURL + "/" + endpoint
+    return finalURL
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DspaceRestRuby::Application.config.secret_key_base = '1312a3611e0491836455d2e87142c76675a3674732e3b6c97a79ed06f3933d9079dd29c722d723fef9f6826f736a92dedb5b09783095c74f3b3fa887c8cc8956'
