490d6d0abf6f6659708d55b596842f343b943c36
Mike Kalvas <kalvas.2@osu.edu>
2014-03-17 15:13:38 -0400

    Adjusts formatting of community controller file

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,42 +1,43 @@
 class CommunityController < HomeController
 
 
-def show
+  # @return [@community]
+  def show
 
-  @community = Community.new
-  url = url_append(params[:id], $baseRestURL + '/communities')
+    @community = Community.new
+    url = url_append(params[:id], $baseRestURL + '/communities')
 
-  rawJSON = RestClient.get url, { :accept => :json}
-  jsonNode = JSON.parse(rawJSON)
+    rawJSON = RestClient.get url, { :accept => :json}
+    jsonNode = JSON.parse(rawJSON)
 
-  @community.id = jsonNode['id'] unless jsonNode['id'].nil?
+    @community.id = jsonNode['id'] unless jsonNode['id'].nil?
 
-  @community.name = jsonNode['name'] unless jsonNode['name'].nil?
+    @community.name = jsonNode['name'] unless jsonNode['name'].nil?
 
-  @community.handle = jsonNode['handle'] unless jsonNode['handle'].nil?
+    @community.handle = jsonNode['handle'] unless jsonNode['handle'].nil?
 
-  @community.type = jsonNode['type'] unless jsonNode['type'].nil?
+    @community.type = jsonNode['type'] unless jsonNode['type'].nil?
 
-  @community.link = jsonNode['link'] unless jsonNode['link'].nil?
+    @community.link = jsonNode['link'] unless jsonNode['link'].nil?
 
-  @community.logo= jsonNode['logo'] unless jsonNode['logo'].nil?
+    @community.logo= jsonNode['logo'] unless jsonNode['logo'].nil?
 
-  @community.logo= jsonNode['parentCommunity'] unless jsonNode['parentCommunity'].nil?
+    @community.logo= jsonNode['parentCommunity'] unless jsonNode['parentCommunity'].nil?
 
-  @community.copyrightText = jsonNode['copyrightText'] unless jsonNode['copyrightText'].nil?
+    @community.copyrightText = jsonNode['copyrightText'] unless jsonNode['copyrightText'].nil?
 
-  @community.introductoryText = jsonNode['introductoryText'] unless jsonNode['introductoryText'].nil?
+    @community.introductoryText = jsonNode['introductoryText'] unless jsonNode['introductoryText'].nil?
 
-  @community.shortDescription = jsonNode['shortDescription'] unless jsonNode['shortDescription'].nil?
+    @community.shortDescription = jsonNode['shortDescription'] unless jsonNode['shortDescription'].nil?
 
-  @community.sidebarText = jsonNode ['sidebarText'] unless jsonNode['sidebarText'].nil?
+    @community.sidebarText = jsonNode ['sidebarText'] unless jsonNode['sidebarText'].nil?
 
-  @community.countItems = jsonNode['countItems'] unless jsonNode['countItems'].nil?
+    @community.countItems = jsonNode['countItems'] unless jsonNode['countItems'].nil?
 
-  @community.collections = jsonNode['collections'] unless jsonNode['collections'].nil?
+    @community.collections = jsonNode['collections'] unless jsonNode['collections'].nil?
 
-  @community.subcommunities = jsonNode['subcommunities'] unless jsonNode['subcommunities'].nil?
+    @community.subcommunities = jsonNode['subcommunities'] unless jsonNode['subcommunities'].nil?
 
-end
+  end
 
 end
