dc3cc5cb36b03e81710a8712a64a7ec74d8beb02
Mike Kalvas <kalvas.2@osu.edu>
2014-03-17 12:28:17 -0400

    Changes home controller to get correct URL's for view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  require 'json'
+  require 'rest_client'
+
+  $baseRestURL = "http://kb.osu.edu/rest"
+
+  def url_append(endpoint, baseURL)
+
+    finalURL = baseURL + "/" + endpoint
+    return finalURL
+
+  end
+
 end
