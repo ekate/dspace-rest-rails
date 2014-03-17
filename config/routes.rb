5cf4c21ecdfd6849716c6036d47ce8dca4e3016b
Mike Kalvas <kalvas.2@osu.edu>
2014-03-17 12:31:22 -0400

    Now links to correct community path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 DspaceRestRuby::Application.routes.draw do
+
+
   get "home/index"
+
+  resources :community
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
