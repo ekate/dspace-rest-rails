d3dd05577c9085394729057b1d5ae7a647dc18a6
Mike Kalvas <kalvas.2@osu.edu>
2014-03-19 13:28:56 -0400

    Adds collection routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ DspaceRestRuby::Application.routes.draw do
 
   resources :community
 
+  resources :collection
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
