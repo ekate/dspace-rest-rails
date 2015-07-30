3f4156ecdc81e80124c6e535e4645751ed8a63bf
Peter Dietz <peter@longsight.com>
2015-07-30 18:02:46 -0400

    Change links

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -5,7 +5,7 @@ class CommunityController < HomeController
   def show
 
     @community = Community.new
-    $endpoint = url_append(params[:id], $base_rest_url + '/communities')
+    $endpoint = url_append(params[:id], $base_rest_url + '/communities/top-communities')
 
     get_json
     parse(@community)
